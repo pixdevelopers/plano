e667a9c7bd43a5b8ab70f166227cb196eea59982
JiyarDev <JiyarDev@yahoo.com>
2018-12-03 05:30:56 +0330

    changes

--- src/routes/info.js
+++ src/routes/info.js
@@ -27,4 +27,27 @@ router.get('/:code', async (req, res, next) => {
   res.send({ client, services });
 });
 
+router.get('/qr/:code', async (req, res, next) => {
+    const client = await Client.findOne({
+      'profile.qrCode': req.params.code
+    }).select('name email avatar description isLocked profile.mobile profile.isParkingAvailble profile.address profile.location profile.webSite profile.qrCode profile.description');
+
+    if (!client)
+        return res.status(404).send('The profile with the given code was not found.');
+    if (client.isLocked)
+        return res.status(401).send('The profile with the given code was deactived.');
+    delete client.isLocked;
+    const services = await ServiceType.find({ client: req.params.id }).select(
+        'name price duration description'
+    );
+    //TODO add rating to each service
+    // for(const item of services){
+    //   const rate = await Service.find({
+    //     serviceType: item._id
+    //   }).select('rating.rank');
+    // }
+
+    res.send({ client, services });
+});
+
 export default router;
